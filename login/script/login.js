939dddfe198283acbbd8b2229bdb0dcbcaa40fcb
arunrajbhar123 <arunrajbhar1998@gmai.com>
2022-03-31 22:41:20 +0530

    update

--- login/script/login.js
+++ login/script/login.js
@@ -10,8 +10,6 @@ function loginfunc() {
     event.preventDefault();
     var useremail = document.querySelector("#email").value;
     var userpassword = document.querySelector("#password").value;
-
-    // console.log(userpassword, useremail);
     var dangeremail = document.querySelector(".email");
     var dangerpassword = document.querySelector(".password");
     var dangernotexist = document.querySelector(".notexist");
@@ -19,17 +17,15 @@ function loginfunc() {
 
         if (el.email == useremail && el.password == userpassword) {
             alert("login sucessfull press ok to shopping")
-            window.location.href = "../register/register.html";
+            window.location.href = "../index.html";
 
-            // console.log(el.email, useremail);
-            // console.log(el.password, userpassword);
         }
         if (el.email != useremail && el.password != userpassword) {
             dangernotexist.style.display = "block";
 
         } else {
             dangernotexist.style.display = "block";
-            // dangernotexist.style.display = "block";
+
         }
         if (useremail == "") {
             dangeremail.style.display = "block";
