cd054e97e25328401b52064cb2de63dfc812c1b9
arunrajbhar123 <arunrajbhar1998@gmai.com>
2022-03-31 19:56:38 +0530

    login section

---
+++ login/script/login.js
@@ -0,0 +1,47 @@
+var redirectregister = document.querySelector("#redirectRegister").addEventListener("click", timemachine);
+var collectregistereddata = JSON.parse(localStorage.getItem("registereduser"));
+
+function timemachine() {
+    window.location.href = "../register/register.html";
+}
+var loginuserdetails = document.querySelector("form").addEventListener("submit", loginfunc);
+
+function loginfunc() {
+    event.preventDefault();
+    var useremail = document.querySelector("#email").value;
+    var userpassword = document.querySelector("#password").value;
+
+    // console.log(userpassword, useremail);
+    var dangeremail = document.querySelector(".email");
+    var dangerpassword = document.querySelector(".password");
+    var dangernotexist = document.querySelector(".notexist");
+    collectregistereddata.map(function(el) {
+
+        if (el.email == useremail && el.password == userpassword) {
+            alert("login sucessfull press ok to shopping")
+            window.location.href = "../register/register.html";
+
+            // console.log(el.email, useremail);
+            // console.log(el.password, userpassword);
+        }
+        if (el.email != useremail && el.password != userpassword) {
+            dangernotexist.style.display = "block";
+
+        } else {
+            dangernotexist.style.display = "block";
+            // dangernotexist.style.display = "block";
+        }
+        if (useremail == "") {
+            dangeremail.style.display = "block";
+        } else {
+            dangeremail.style.display = "none";
+        }
+        if (userpassword == "") {
+            dangerpassword.style.display = "block";
+        } else {
+            dangerpassword.style.display = "none";
+        }
+    });
+
+
+}
