41fb40a55a0337180ee61e057a634cd65f8edcac
bishnu05 <101577876+bishnu05@users.noreply.github.com>
2022-04-02 17:10:34 +0530

    Add files via upload

---
+++ brands/sale.js
@@ -0,0 +1,325 @@
+var saleData = [
+
+    {
+      img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11219032-2074897166624079.jpg",
+      name: "Chantecaille Bio Lifting Mask - 50ml",
+      info: "Save 25%",
+      actualprice :"165.00",
+      price: "123.75",
+    },
+    
+    {
+      img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12593925-1754871030163732.jpg",
+      name: "Slip Reusable Face Covering (VariousColors)",
+      info: "$15 Slip Gift",
+      price: "19.50",
+    },
+    
+    {
+      img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289657-9384876690289403.jpg",
+      name: "SkinMedica AHA/BHA Exfoliating Cleanser(6oz)",
+      info: "10% off SkinMedica $162 Gift|",
+      actualprice :"47.00",
+      price: "42.30",
+    },
+    {
+      img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289679-1194871019622113.jpg",
+      name: "SkinMedica Scar Recovery Gel with Centelline (20z) ",
+      info: "10% off SkinMedica + $162 Gift",
+      actualprice :"102.00",
+      price: "91.80",
+    },
+    {
+      img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11396212-1194870995675144.jpg",
+      name: "SkinMedica LYTERA 2.0 PigmentCorrecting Serum",
+      info: "10% off SkinMedica + $162 Gift",
+      actualprice :"154.00",
+      price: "138.60",
+    },
+   {
+      img_url:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13485839-1944912987170825.jpg",
+      name : "The Ordinary Natural Moisturizing Factorsand HA 100ml (Three Pack)" ,
+      info: "Save 20%",
+      actualprice:"23.10",
+      price : "18.48" , 
+   },
+ {
+   img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13280903-3884894825427088.jpg",
+   name : "Nuface Fix Smooth And Tighten Gift Set(Worth $159.00)" ,
+   info: "SHOP ALL NUFACE" ,
+   actualprice:"139.00",
+   price : "97.30" ,
+   },
+ 
+ {
+   img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12684049-5244808338974933.jpg",
+   name : "Oh K! Chok Chok Illuminating Serum",
+   info: "Save 50%",
+   actualprice:"15.00",
+   price : "7.50" , 
+  },
+  {
+      img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289682-1284871019539829.jpg",
+      name : "SkinMedica Retinol Complex 1.0 (10z)",
+      info: "10% off SkinMedica+$162 Gift",
+      actualprice:"93.00",
+      price : "83.70" , 
+     },
+ 
+ {img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289678-1494870995623922.jpg",
+ name : "SkinMedica TNS Recovery Complex 1 oz(Worth $285)",
+ info: "l0% off SkinMedica+$162 Gift",
+ actualprice:"220.00",
+ price : "198.00" , 
+},
+
+   {img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11574164-8604871030120307.jpg",
+     name : "Decorté Vita De Rêve Lotion 10.1 f.oz/300ml (Worth $90)",
+      info: "$33 Decorté Gift",
+      actualprice:"75.00",
+      price : "56.75" , 
+     },
+ 
+  {img_url: "https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11995336-5244635651652236.jpg",
+  name : "SkinMedica TNS Eye Repair (0.5oz)",
+  info: "l0% off SkinMedica+$162 Gift",
+  actualprice:"102.00",
+  price : "91.80" , 
+ },
+ 
+ {img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289683-2074871019620370.jpg",
+ name : "Avène Thermal Spring Water 5.29oz",
+ info: "Save 25%",
+ actualprice:"14.00",
+ price : "10.50" , 
+},
+ {img_url:"https://static.thcdn.com/images/xsmall/webp//productimg/original/11286359-1144866308334354.jpg",
+ name : "SkinMedica Essential Defense Mineral Shield Broad Spectrum SPF 32 Tinted (1.85oz) (1.8502)",
+ info: "l0% off SkinMedica+$162 Gift",
+ actualprice:"38.00",
+ price : "34.20" , 
+},
+ 
+ {img_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11290741-2554870995987858.jpg",
+ name : "Dr. PAWPAW Hot Pink Balm 25ml",
+ info: "Save 50%",
+ actualprice:"8.00",
+ price : "4.00" , 
+},
+ 
+ {img_url: "https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/12312417-1384720922171493.jpg",
+ name : "SkinMedica Retinol Complex 0.5 (10z)",
+ info: "l0% off SkinMedica+$162 Gift",
+ actualprice:"78.00",
+ price : "70.20" , 
+},
+ 
+ {img_url: "https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11289796-1434415915963553.jpg",
+ name : "South Beach Skin Solutions Gel for Sensitive Areas",
+ info: "Save 30%",
+ actualprice:"50.00",
+ price : "35.00" , 
+},
+ 
+{img_url: "https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11289661-1794453993724103.jpg",
+name : "SkinMedica Facial Cleanser",
+info: "Save 30%",
+actualprice:"38.00",
+price : "34.20" , 
+},
+ 
+{img_url: "https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/12682868-2054802894273517.jpg",
+name : "Decorté Botanical Skincare Collection 6Piece Set (Worth $161.00)",
+info: "$33 Decorté Gift",
+actualprice:"115.00",
+price : "80.50" , 
+},
+{img_url: "https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/12607212-1084784265954258.png",
+name : "TONYMOLY Aloe Soothing Gel 2.5 oz",
+info: "Save 50%",
+actualprice:"10.00",
+price : "5.00" , 
+},
+{img_url: "https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/11422048-1984789877043717.jpg",
+name : "Pestle &Mortar Erase Cleansing Bal 100ml",
+info: "Save 50%",
+actualprice:"59.00",
+price : "29.50" , 
+},
+ 
+{img_url: "https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11995335-1374635651521394.jpg",
+name : "Decorté Prime Latte Moisturizer 10.1 f1.oz/300ml (Worth $90)",
+info: "Save 50%",
+actualprice:"50.00",
+price : "35.00" , 
+},
+ 
+{img_url: "https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11289796-1434415915963553.jpg",
+name : "South Beach Skin Solutions Gel for Sensitive Areas",
+info: "Save 30%",
+actualprice:"75.00",
+price : "56.25" , 
+},
+{img_url: "https://static.thcdn.com/images/xsmall/webp/productimg/1600/1600/11294941-1314442122315542.jpg",
+name : "Serious Skincare Retexturizing Glycolic Pads",
+info: "Save 50%",
+actualprice:"34.50",
+price : "17.25" , 
+},
+ ]
+
+
+var cartData= JSON.parse(localStorage.getItem("cart"))||[];
+
+window.addEventListener("load",function(){
+displayData(saleData)
+})
+
+ function pricesort(){
+   var selected=document.querySelector("#newprice").value;
+
+   if(selected=="lth"){
+     saleData.sort(function(a,b){
+       return a.price-b.price;
+     });
+     displayData(saleData);
+   }
+
+   if(selected=="htl"){
+    saleData.sort(function(a,b){
+      return b.price-a.price;
+    });
+    displayData(saleData);
+  }
+
+ }
+ 
+ function namesort(){
+   var selected= document.querySelector("#Alphabets").value;
+
+   if(selected=="asc"){
+     saleData.sort(function(a,b){
+       if(a.name>b.name) return 1;
+       if(a.name<b.name) return -1;
+       return 0;
+     });
+     displayData(saleData);
+   }
+
+   if(selected=="dsc"){
+    saleData.sort(function(a,b){
+      if(a.name>b.name) return -1;
+      if(a.name<b.name) return 1;
+      return 0;
+    });
+    displayData(saleData);
+  }
+ }
+
+
+//  window.addEventListener("load",function(){
+//   var callbutn = document.querySelectorAll(".btn")
+//   // console.log(callbutn)
+//   for (var i = 0;i<callbutn.length-1;i++)
+//   {
+//       callbutn[i].addEventListener("click",chngpage)
+//   }
+//  })
+
+
+//  function chngpage(e)
+//  { 
+//      var data1 = JSON.parse(localStorage.getItem("localdata"))
+     
+//   //    console.log(e.target.innerText)
+//      var val = e.target.innerText
+//      var val1= e.target.value
+ 
+//   var a = 0
+//   var b= 0
+//    if (val == "1")
+//    {
+//        a =8
+//        b=16
+       
+//    }
+//   else if (val == "2"){
+//       a = 17
+//       b=33
+//   }
+  
+//   if(val1 =="minus")
+//   {
+//       a = 0
+//       b=8
+//   }
+
+//    var splicedata = data1.slice(a,b)
+//    run(splicedata)
+  
+//  }
+
+//  var data2 = JSON.parse(localStorage.getItem("localdata"))
+//  var displaydata = data2.slice(0,12)
+//    run(data)
+//    function  displayData(saleData){
+//   document.querySelector(".mainproduct").innerHTML=""
+//     saleData.map(function(el){
+//         var div = document.createElement("div")
+//         div.setAttribute("class","product");
+//         var img = document.createElement("img")
+//         img.src=el.image
+//         var h2 = document.createElement("h2")
+//         h2.innerText=el.name
+//         var p = document.createElement("p")
+//         p.innerText=el.info
+//         var span = document.createElement("h3")
+//         span.innerText="$"+el.actualprice
+//         var dam = document.createElement("span")
+//         dam.innerText="$"+el.price
+//         var button = document.createElement("button")
+//          button.innerText="Quick Buy"
+
+//          button.addEventListener("click", function() {
+//           givintstro(el);
+//       });
+//         div.append(img,h2,p,span,dam,button)
+//         document.querySelector(".mainproduct").append(div)
+  
+//     })
+//    }
+ 
+//    function givintstro(el) {
+//     arr.push(el);
+//     localStorage.setItem("cartadddatabase", JSON.stringify(arr));
+// window.location.reload();
+// }
+
+saleData.map(function(el) {
+var box = document.createElement("div");
+var img = document.createElement("img");
+img.src = el.img_url;
+var name = document.createElement("p");
+name.innerText = el.name;
+var price = document.createElement("p");
+price.innerText = el.price;
+var btn = document.createElement("button");
+btn.innerText = "add bag";
+btn.addEventListener("click", function() {
+    givintstro(el);
+});
+box.append(img, name, price, btn);
+
+
+document.querySelector(".container").append(box);
+});
+var arr = JSON.parse(localStorage.getItem("cartadddatabase")) || [];
+
+function givintstro(el) {
+arr.push(el);
+localStorage.setItem("cartadddatabase", JSON.stringify(arr));
+window.location.reload();
+}
+// localStorage.clear();
+
+
